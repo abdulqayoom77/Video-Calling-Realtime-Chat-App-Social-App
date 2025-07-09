2c7d8e13286b6701b7071a1647f80d9a202a1c5a
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-07-10 04:52:57 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
+  res.send('Hello from Express!');
 });
 
 export default app;
